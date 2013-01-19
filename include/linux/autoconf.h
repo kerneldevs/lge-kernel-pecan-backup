8569f022029a9af2df1035b74c9474e9ce2847f7
RonGokhale <cips173@gmail.com>
2013-01-20 00:35:28 +0530

    [P350][C550] Updated and add defconfig

--- include/linux/autoconf.h
+++ include/linux/autoconf.h
@@ -1,7 +1,7 @@
 /*
  * Automatically generated C config: don't edit
  * Linux kernel version: 2.6.35.10
- * Sat Jan 19 23:04:40 2013
+ * Sun Jan 20 00:32:22 2013
  */
 #define AUTOCONF_INCLUDED
 #define CONFIG_VIDEO_V4L1_COMPAT 1
