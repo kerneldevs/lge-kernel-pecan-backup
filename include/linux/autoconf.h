2a0d729c0b5438a13891ca9b82a8a60fe0487882
RonGokhale <cips173@gmail.com>
2013-01-28 18:16:27 +0530

    update autoconf.h
    Change-Id: Ie6ad9c8315bfd6b834ed83cf3fda4062ca45f9bb

--- include/linux/autoconf.h
+++ include/linux/autoconf.h
@@ -1,7 +1,7 @@
 /*
  * Automatically generated C config: don't edit
  * Linux kernel version: 2.6.35.10
- * Sat Jan 26 11:22:47 2013
+ * Mon Jan 28 17:58:52 2013
  */
 #define AUTOCONF_INCLUDED
 #define CONFIG_VIDEO_V4L1_COMPAT 1
