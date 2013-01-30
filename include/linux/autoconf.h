8c0140385f725749cced9482b2600eb727cd1fea
RonGokhale <cips173@gmail.com>
2013-01-30 14:16:39 +0530

    [P350] Updated defconfig
    Change-Id: I685af9791fde37958bd61054f915a667585fe3e9

--- include/linux/autoconf.h
+++ include/linux/autoconf.h
@@ -1,7 +1,7 @@
 /*
  * Automatically generated C config: don't edit
  * Linux kernel version: 2.6.35.10
- * Wed Jan 30 13:08:56 2013
+ * Wed Jan 30 14:11:42 2013
  */
 #define AUTOCONF_INCLUDED
 #define CONFIG_VIDEO_V4L1_COMPAT 1
