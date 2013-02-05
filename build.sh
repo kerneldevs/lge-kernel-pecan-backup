cdca41ca3183664bfc3a8bce440de88215f6f259
RonGokhale <cips173@gmail.com>
2013-02-05 14:03:14 +0530

    [COMMON] Update build script for PecanCM
    Change-Id: I0efae7d7a08a9ae432cecb435bee59ee8b940c85

--- build.sh
+++ build.sh
@@ -11,9 +11,18 @@ daytime=$(date +%d"-"%m"-"%Y"_"%H"-"%M)
 location=.
 vendor=lge
 
-# changed antdking "removed options. it's a device specific kernel" 04/02/13
-export target=p350
-export defconfig=cyanogenmod_p350_defconfig
+# changed Ron "Readded options. This have hazel board as well" 05/02/13
+if [ -z $target ]; then
+echo "choose your target device"
+echo "1) p350"
+echo "2) c500"
+read -p "1/2: " choice
+case "$choice" in
+1 ) export target=p350 ; export defconfig=cyanogenmod_p350_defconfig;;
+2 ) export target=c500 ; export defconfig=cyanogenmod_hazel_defconfig;;
+* ) echo "invalid choice"; sleep 2 ; ./build.sh;;
+esac
+fi # [ -z $target ]
 
 if [ -z $compiler ]; then
 if [ -f ../arm-eabi-4.6/bin/arm-eabi-* ]; then
@@ -59,7 +68,7 @@ cp arch/arm/boot/zImage zip-creator/kernel
 find . -name *.ko | xargs cp -a --target-directory=zip-creator/system/lib/modules/
 
 
-zipfile="lge-3.0.x-$target-$daytime.zip"
+zipfile="PecanCM.x-$target-$daytime.zip"
 cd zip-creator
 rm -f *.zip
 zip -r $zipfile * -x *kernel/.gitignore*
